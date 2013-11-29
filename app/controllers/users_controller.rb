b1b31a9bf333a3f28a997af73a13816a08d5abb9
zumgugger <markus.gugger@gmail.com>
2013-11-29 17:42:53 +0100

    correct user requrement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate, :only => [:edit, :update]
+  before_action :correct_user, :only => [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -46,6 +47,11 @@ class UsersController < ApplicationController
   
   def authenticate
     deny_access unless signed_in?
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless @user == current_user
   end
    
 end
