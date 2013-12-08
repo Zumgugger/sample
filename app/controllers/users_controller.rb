ee9eef2cdeca43955c849ddc54d3f48be069409a
zumgugger <markus.gugger@gmail.com>
2013-12-09 00:49:23 +0100

    following followers pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_action :authenticate, :except => [:show, :new, :create ]
   before_action :correct_user, :only => [:edit, :update]
   before_action :admin_user,   :only => :destroy
   
@@ -12,6 +12,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
     @microposts = @user.microposts.paginate(:page => params[:page])
+  end
+  
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'   
+  end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
   end
 
   def new
